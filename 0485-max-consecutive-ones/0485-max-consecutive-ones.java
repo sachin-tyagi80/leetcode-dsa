b4c803c4d875fa99b9574dc68928d7dc5cbb1da4
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-06 22:58:30 +0530

    Time: 3 ms (60.47%), Space: 52.4 MB (65.55%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -8,15 +8,10 @@ class Solution {
                 maxCount = Math.max(maxCount,count);
             }else{
                 count = 0;
+
             }
         }
         return maxCount;
         
     }
 }
-
-
-// "I traverse the array once while maintaining two variables: 
-// one for the current consecutive count of 1's and another for the maximum count found so far. If the current element is 1, 
-// I increment the count and update the maximum. If it is 0, I reset the count because the consecutive sequence breaks. 
-// This solution runs in O(n) time and O(1) extra space."
