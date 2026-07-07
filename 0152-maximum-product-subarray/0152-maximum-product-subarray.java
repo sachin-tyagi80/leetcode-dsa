97168fda39978925d45f982bcd4b426fa4aa7bbc
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-07 17:37:53 +0530

    Time: 2 ms (70.74%), Space: 47.1 MB (87.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,27 +1,20 @@
 class Solution {
     public int maxProduct(int[] nums) {
-
-        int maxProduct = nums[0];
-        int minProduct = nums[0];
+        int max = nums[0];
+        int min = nums[0];
         int ans = nums[0];
 
         for(int i =1;i<nums.length;i++){
             if(nums[i]<0){
-                int temp = maxProduct;
-                maxProduct = minProduct;
-                minProduct = temp;
+                int temp = max;
+                max = min;
+                min = temp;
             }
-            maxProduct = Math.max(nums[i],nums[i]*maxProduct);
-            minProduct = Math.min(nums[i],nums[i]*minProduct);
-            ans = Math.max(ans,maxProduct);
+            max = Math.max(nums[i],nums[i]*max);
+            min = Math.min(nums[i],nums[i]*min);
+            ans = Math.max(ans,max);
         }
         return ans;
+        
     }
 }
-
-
-
-// "I maintain both the maximum and minimum product ending at the current index because a negative number can turn the smallest product into the largest one. 
-// Whenever I encounter a negative element, I swap the current maximum and minimum products before updating them. 
-// Then I update the answer with the maximum product seen so far. 
-// Since I process each element only once, the time complexity is O(n) and the space complexity is O(1)."
