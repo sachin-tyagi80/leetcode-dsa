4386aeda12ddd8fc14dce72760188c6f7787b997
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-10 23:01:52 +0530

    Time: 2 ms (99.33%), Space: 47.4 MB (10.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,21 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
-                }
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+
+            map.put(nums[i], i);
         }
-        // If no valid pair is found, return an empty array instead of null
-        return new int[] {};
+
+        return new int[0];
     }
 }
