0590265cfa06a62a3a41a017bb35b3899b468305
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-15 23:36:24 +0530

    Time: 53 ms (56.31%), Space: 95.6 MB (69.06%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -4,14 +4,17 @@ class Solution {
             return 0;
         }
 
+        // Sort balloons based on ending coordinate
         Arrays.sort(points, (a, b) -> Long.compare((long) a[1], (long) b[1]));
+
+        // First arrow at first balloon's end
         int arrows = 1;
         long arrowPosition = points[0][1];
 
-        
+        // Traverse remaining balloons
         for (int i = 1; i < points.length; i++) {
 
-            
+            // Current balloon cannot be burst
             if (points[i][0] > arrowPosition) {
 
                 arrows++;
