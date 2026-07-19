2f859e51e7092b3d763f92ef29b592b743e49b80
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-19 21:02:38 +0530

    Improve readability of balancedStringSplit method
    Refactor the balanced string split method for clarity.

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -2,14 +2,14 @@ class Solution {
     public int balancedStringSplit(String s) {
         int balance = 0;
         int count = 0;
-        for(char ch : s.toCharArray()){
-            if(ch == 'R'){
+        for(char ch : s.toCharArray()){    // Traverse the string
+            if(ch == 'R'){                 // Increase balance for R
                 balance++;
             }
-            else{
+            else{                           // Decrease balance for L
                 balance--;
             }
-            if(balance == 0){
+            if(balance == 0){              // Current substring is balanced
                 count++;
             }
         }
@@ -17,3 +17,9 @@ class Solution {
         
     }
 }
+
+
+
+// I use a Greedy approach with a balance counter. Whenever I see 'R', I increment the balance, and whenever I see 'L',
+// I decrement it. As soon as the balance becomes zero, it means the current substring has an equal number of L's and R's,
+// so I immediately split the string. This greedy choice maximizes the number of balanced substrings.
