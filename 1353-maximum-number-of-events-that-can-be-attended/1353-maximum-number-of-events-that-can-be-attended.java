3d86080ede7fbb216deb0ebf7edf2e2c6defd35c
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-20 22:40:29 +0530

    Add comments explaining the greedy strategy for events
    Added explanation of the greedy approach used to solve the problem.

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -27,3 +27,10 @@ class Solution {
         
     }
 }
+
+
+
+// "I solved this problem using a Greedy approach with a Min Heap. First, I sorted all events by their start day. Then I simulated each day. 
+// On every day, I added all events that started on that day into a priority queue ordered by their end day. Before attending an event, 
+// I removed all expired events. Finally, I always attended the event with the earliest end day because delaying such an event could make 
+// it impossible to attend later. This greedy strategy maximizes the number of events attended."
