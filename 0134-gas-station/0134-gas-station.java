f3d241b9507d378b9f6cbc69b25a35e262aa7cb7
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-21 23:43:25 +0530

    Fix formatting and clean up comments in gas station solution
    Added a newline at the end of the file and removed unnecessary comments.

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -20,3 +20,9 @@ class Solution {
         return start;
     }
 }
+
+
+// "I used a Greedy approach. First, I checked whether the total amount of gas is at least equal to the total travel cost. If not, completing the circuit
+// is impossible. Then I traversed the stations while maintaining the current fuel in the tank. Whenever the tank became negative,
+// it meant the current starting station and all stations before the current one could not be valid starting points, so I reset the tank
+// and moved the starting station to the next index. This gives an O(n) solution."
