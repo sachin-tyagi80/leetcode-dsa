b2b2529c54ac614f68d269e5a2ff8f01b570f163
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-22 23:34:11 +0530

    Time: 2 ms (98.71%), Space: 72.3 MB (85.27%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,37 +1,56 @@
 class Solution {
+
     public boolean lemonadeChange(int[] bills) {
+
+        // Count of $5 and $10 bills
         int five = 0;
         int ten = 0;
-        for(int bill : bills){
-            if(bill == 5){
+
+        // Process each customer
+        for (int bill : bills) {
+
+            // Customer pays $5
+            if (bill == 5) {
+
                 five++;
             }
 
-            else if(bill == 10){
-                if(five == 0){
-                  return false;
+            // Customer pays $10 
+            else if (bill == 10) {
+
+                // Need one $5 as change
+                if (five == 0) {
+                    return false;
                 }
+
                 five--;
                 ten++;
             }
-            else{
-                if(ten>0 &&  five>0){
+
+            // Customer pays $20
+            else {
+
+                // Prefer giving one $10 and one $5
+                if (ten > 0 && five > 0) {
+
                     ten--;
                     five--;
                 }
-                else if(five >=3){
+
+                // Otherwise give three $5 bills // 5,5,5
+                else if (five >= 3) {
+
                     five -= 3;
                 }
-                else{
+
+                // Change not possible
+                else {
+
                     return false;
                 }
-                            
-                            
             }
         }
 
         return true;
-
-        
     }
 }
