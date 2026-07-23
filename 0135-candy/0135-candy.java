b566b8963e80fb6bf4da6dca9f37b97da8686682
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-23 19:47:18 +0530

    Time: 3 ms (86.79%), Space: 47.5 MB (84.92%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -3,21 +3,23 @@ class Solution {
         int n = ratings.length;
         int[] candies = new int[n];
         Arrays.fill(candies,1);
+
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                candies[i] = candies[i-1]+1;
+                candies[i] = candies[i-1] + 1;
             }
         }
-        for(int i = n-2;i>=0;i--){
+        for(int i= n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
                 candies[i] = Math.max(candies[i],candies[i+1]+1);
             }
         }
-        int total = 0;
+        int total =0;
         for(int candy : candies){
             total += candy;
         }
+
         return total;
-        
+
     }
 }
