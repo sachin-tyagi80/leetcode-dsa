77c6012fd06cfd568cc43a2879bc2e65c0498403
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-24 23:14:58 +0530

    Enhance code readability with comments in canJump
    Added comments to clarify the purpose of variables and logic in the canJump method.

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach =0;
+        int maxReach =0;  // Maximum index we can reach
         for(int i=0;i<nums.length;i++){
-            if(i>maxReach){
+            if(i>maxReach){    // Current index is unreachable
                 return false;
             }
-            maxReach = Math.max(maxReach,i+nums[i]);
+            maxReach = Math.max(maxReach,i+nums[i   // Update the farthest reachable index
 
         }
         return true;
         
     }
 }
+
+
+// Interview Explanation (English)
+
+// "I use a Greedy approach. I maintain the farthest index that can be reached so far using a variable maxReach. While traversing the array,
+// if the current index is greater than maxReach, it means that index is unreachable, so I return false. Otherwise, I update maxReach as max(maxReach,i + nums[i]).
+// If I finish the traversal, it means the last index is reachable."
