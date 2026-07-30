117b9f0fe00ae22f219edee977320688d09db2e9
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-07-30 21:24:21 +0530

    Time: 0 ms (100%), Space: 43.1 MB (79.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,10 +1,12 @@
 class Solution {
     public int minimumPushes(String word) {
-        int push = 0;
-        int n= word.length();
-        for(int i=0;i<n;i++){
-            push += (i/8)+1;
+        int push =0;
+        int n = word.length();
+        for(int i=0;i<n;i++){   // Traverse every character
+            push += (i/8)+1;   // Every group of 8 letters requires one extra push
         }
+
         return push;
+        
     }
 }
