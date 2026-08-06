7ce4e2a9b843133ff23ad156ca5f7a56fb1a8bab
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-08-06 12:57:07 +0530

    Time: 1 ms (100%), Space: 42.8 MB (30.89%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -3,15 +3,16 @@ class Solution {
         while(true){
             int product = 1;
             int num = n;
-            while(num>1){
+
+            while(num>0){  // Calculate digit product
                 product *= (num%10);
                 num /= 10;
+
             }
 
-            if(product % t == 0){
+            if(product % t == 0){  // Check divisibility
                 return n;
             }
-
             n++;
         }
         
