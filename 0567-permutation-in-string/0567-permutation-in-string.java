3ca0b7e53dfc72fd2fc99965a8ae387d241c5c2f
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-08-07 15:05:38 +0530

    Time: 10 ms (33.63%), Space: 43.9 MB (74.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,38 @@
 class Solution {
+
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()){
+
+        if (s1.length() > s2.length()) {
             return false;
         }
 
         int[] need = new int[26];
         int[] window = new int[26];
 
-        for(char ch : s1.toCharArray()){
-            need[ch-'a']++;
+        // Frequency of s1
+        for (char ch : s1.toCharArray()) {
+            need[ch - 'a']++;
         }
-        int l = 0;
-        for(int r =0;r<s2.length();r++){
-            window[s2.charAt(r) - 'a']++;
-            if(r-l +1 >s1.length()){
-                window[s2.charAt(l)-'a']--;
-                l++;
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+
+            window[s2.charAt(right) - 'a']++;
+
+            // Window size greater than s1
+            if (right - left + 1 > s1.length()) {
+
+                window[s2.charAt(left) - 'a']--;
+                left++;
             }
 
-        
-            if(Arrays.equals(need,window)){
+            // Compare both frequency arrays
+            if (Arrays.equals(need, window)) {
                 return true;
             }
         }
+
         return false;
     }
 }
