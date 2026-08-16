3dfbc34689f17996dcb0c064a469ca2ca98489f2
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-08-16 17:04:44 +0530

    Time: 0 ms (100%), Space: 46.5 MB (11.63%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,21 +1,43 @@
 class Solution {
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+
+        // Total number of rooms
         int n = rooms.size();
-        boolean[] vis =  new boolean[n];
-        dfs(0,rooms,vis);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
+
+        // visited[i] tells whether room i has been visited
+        boolean[] visited = new boolean[n];
+
+        // We can enter room 0 initially,
+        // so start DFS from room 0
+        dfs(0, rooms, visited);
+
+        // Check whether every room was visited
+        for (int i = 0; i < n; i++) {
+
+            // If any room is not visited,
+            // we cannot visit all rooms
+            if (!visited[i]) {
                 return false;
             }
-
         }
+
+        // All rooms were visited
         return true;
     }
-    private void dfs(int curr,List<List<Integer>> rooms, boolean[] vis){
-        vis[curr] = true;
-        for(int key : rooms.get(curr)){
-            if(!vis[key]){
-                dfs(key,rooms,vis);
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+
+        // Mark current room as visited
+        visited[room] = true;
+
+        // Get all keys available in the current room
+        for (int key : rooms.get(room)) {
+
+            // If the room for this key is not visited,
+            // visit that room using DFS
+            if (!visited[key]) {
+                dfs(key, rooms, visited);
             }
         }
     }
