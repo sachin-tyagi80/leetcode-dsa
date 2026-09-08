31df9ebb175a7fe671a696555f533a62425a89cf
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-09-08 22:19:19 +0530

    Time: 8 ms (8.47%), Space: 42.9 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,9 +1,12 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<1000){
-            return 0;
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(i>=1000){
+                count++;
+            }
         }
-        return n-999;
-        
+
+        return count;
     }
 }
